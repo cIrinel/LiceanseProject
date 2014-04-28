42a18fc4a489bfde07cf860b045ec033b82d48e7
iCraiu <craiu.irinel@gmail.com>
2014-04-29 00:34:07 +0300

    added image decomposer: accepts as input some image and outputs three bufferedImages, each bufferedImage is the C/M/Yscale representation of the original image.

--- src/test/java/com/irinel/craiu/liceanse/halftone/impl/ColorDecomposerTest.java
+++ src/test/java/com/irinel/craiu/liceanse/halftone/impl/ColorDecomposerTest.java
@@ -1,47 +1,44 @@
 package com.irinel.craiu.liceanse.halftone.impl;
 
-import com.irinel.craiu.liceanse.imageutils.DecomposedImage;
+import com.google.common.io.Resources;
+import com.irinel.craiu.liceanse.imageutils.DecomposedCMYImage;
 import org.junit.Before;
 import org.junit.Test;
-
 import javax.imageio.ImageIO;
-
 import static org.junit.Assert.*;
-
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 
 public class ColorDecomposerTest {
-    //TODO: add guava to this project to manage resources better---manu
-    private static final String IMAGE_FILE_NAME = "src/main/resources/hdImage.jpg";
+
+    private static final String IMAGE_FILE_NAME = "hdImage.jpg";
     private ColorDecomposer colorDecomposer;
     private BufferedImage bufferedImage;
-    private DecomposedImage decomposedImage;
+    private DecomposedCMYImage decomposedImage;
 
     @Before
     public void setUp() throws IOException {
         colorDecomposer = new ColorDecomposer();
-        bufferedImage = ImageIO.read(new File(IMAGE_FILE_NAME));
-        decomposedImage = colorDecomposer.decomposeImageToPrimaryColors(bufferedImage);
+        bufferedImage = ImageIO.read(Resources.getResource(IMAGE_FILE_NAME));
+        decomposedImage = colorDecomposer.decomposeImageToCNYColors(bufferedImage);
     }
 
 
     @Test
-    public void imageDecomposesToFirstPrimaryColor() throws Exception {
-        assertEquals(decomposedImage.getImageInPrimaryColor1() != null, true);
+    public void imageDecomposesToRedScale() throws Exception {
+        assertEquals(decomposedImage.getCyanScaleImage() != null, true);
     }
 
     @Test
-    public void imageDecomposesToSecondPrimaryColor() throws Exception {
-        assertEquals(decomposedImage.getImageInPrimaryColor2() != null, true);
+    public void imageDecomposesToGreenScale() throws Exception {
+        assertEquals(decomposedImage.getMangentaScaleImage() != null, true);
     }
 
     @Test
-    public void imageDecomposesToThirdPrimaryColor() throws Exception {
-        assertEquals(decomposedImage.getImageInPrimaryColor3() != null, true);
+    public void imageDecomposesToBlueScale() throws Exception {
+        assertEquals(decomposedImage.getMangentaScaleImage() != null, true);
+
     }
 }
